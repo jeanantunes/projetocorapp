3683d49ad45ae0df466185852dcc451c97674091
Diego Paraizo <diegodiasparaizo@gmail.com>
2018-02-24 19:59:58 -0300

    controllers beneficiarios

---
+++ VendasOdontoPrev/app/src/main/assets/app/pmeBenefController.js
@@ -0,0 +1,86 @@
+﻿$(document).ready(function () {
+    carregarLista();
+});
+
+function carregarLista() {
+
+    var proposta = get("proposta");
+
+    $("#razaoSocial").html(proposta.razaoSocial);
+    $("#cnpjEmpresa").html(proposta.cnpj);
+
+}
+
+function salvarBenef() {
+    if ($("#nome-beneficiario").val() == "") {
+        swal("Ops!", "Preencha o Nome", "error");
+        return;
+    }
+
+    if ($("#radio-1").is(":checked") == false && $("#radio-2").is(":checked") == false) {
+        swal("Ops!", "Selecione o Sexo", "error");
+        return;
+    }
+
+    if ($(".nascimento").val() == "") {
+        swal("Ops!", "Preencha a Data de Nascimento", "error");
+        return;
+    }
+
+    if ($(".cpf").val() == "") {
+        swal("Ops!", "Preencha o CPF", "error");
+        return;
+    }
+
+    if ($(".nome-mae").val() == "") {
+        swal("Ops!", "Preencha o Nome da Mãe", "error");
+        return;
+    }
+
+    if ($(".cep").val() == "") {
+        swal("Ops!", "Preencha o CEP", "error");
+        return;
+    }
+
+    var proposta = get("proposta");
+    var benef = getRepository("beneficiario");
+
+    benef.nome = $("#nome-beneficiario").val();
+    benef.nomeMae = $("#nome-beneficiario").val();
+
+    if ($("#radio-1").is(":checked") == true) {
+        benef.sexo = $("#radio-1").val();
+    }
+    else {
+        benef.sexo = $("#radio-2").val();
+    }
+
+    benef.dataNascimento = $(".nascimento").val();
+    benef.cpf = $(".cpf").val();
+    benef.cnpj = proposta.cnpj;
+    benef.endereco.cep = $(".cep").val();
+    benef.cdPlano = $(".plano").val();
+
+    var beneficiarios = get("beneficiarios");
+
+    if (beneficiarios == null) {
+        beneficiarios = [];
+    }
+
+    beneficiarios.push(benef);
+
+    put("beneficiarios", JSON.stringify(beneficiarios));
+
+    swal({
+        title: "Feito!",
+        text: "Beneficiário cadastrado com sucesso.",
+        type: "success",
+        showCancelButton: false,
+        confirmButtonText: 'OK',
+        closeOnConfirm: false,
+        closeOnCancel: false
+    },
+        function (isConfirm) {
+            window.history.go(-1);
+        });    
+}
---
+++ VendasOdontoPrev/app/src/main/assets/app/pmeListaBenefController.js
@@ -0,0 +1,55 @@
+﻿$(document).ready(function () {
+    carregarLista();
+});
+
+function carregarLista() {
+
+    var proposta = get("proposta");
+
+    $("#razaoSocial").html(proposta.razaoSocial);
+    $("#cnpjEmpresa").html(proposta.cnpj);
+
+    var beneficiarios = get("beneficiarios");
+
+    if (beneficiarios != null) {
+        $("#lista").html("");
+    }
+
+    $.each(beneficiarios, function (i, item) {
+        var benef = getComponent("beneficiario");
+        benef = benef.replace("{CPF}", item.cpf);
+        benef = benef.replace("{CPF-BT}", item.cpf);
+        benef = benef.replace("{NOME}", item.nome);
+        benef = benef.replace("{DEPENDENTES}", item.dependentes.length);
+
+        $("#lista").append(benef);
+    });
+
+    if (beneficiarios.length == 0) {
+        $("#lista").html("Você ainda não possui beneficiários cadastrados");
+    }
+
+}
+
+function excluirBenef(obj) {
+
+    var container = $(".div-excluir[data-id='" + $(obj).attr("data-id") + "']");
+    var beneficiarios = get("beneficiarios");
+
+    var beneficiariosExcetoExcluido = beneficiarios.filter(function (x) { return x.cpf != container.attr("data-id") });
+    beneficiarios = [];
+    $.each(beneficiariosExcetoExcluido, function (i, item) {
+        beneficiarios.push(item);
+    });
+
+    put("beneficiarios", JSON.stringify(beneficiarios));
+    container.remove();
+
+    if ($("#lista").length == 0) {
+        $("#lista").html("Você ainda não possui beneficiários cadastrados");
+    }
+
+    if (beneficiarios.length == 0) {
+        $("#lista").html("Você ainda não possui beneficiários cadastrados");
+    }
+}
