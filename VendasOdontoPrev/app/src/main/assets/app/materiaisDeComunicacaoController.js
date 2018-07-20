ceeda23ffc6d192dc8794f7df3e167545c884723
Yago Almeida Santos <yalmeida@vectoritcgroup.com>
2018-07-20 16:03:45 -0300

    [COR-443] Removido mocks dos servicos de materiais

--- VendasOdontoPrev/app/src/main/assets/app/materiaisDeComunicacaoController.js
+++ VendasOdontoPrev/app/src/main/assets/app/materiaisDeComunicacaoController.js
@@ -3,6 +3,10 @@
     //setarItensSlick();
 
     preencherMateriaisDivulgacao();
+
+    $("#myModal").on("hidden.bs.modal", function () {
+        $('body').removeClass('stop-scrolling');
+    });
     
 });
 
@@ -10,13 +14,11 @@ function buscarMateriaisDivulgacao(callback, token) {
 
     $.ajax({
         async: true,
-        //url: URLBase + "/corretorservicos/1.0/dashboardPropostaPME/" + statusTodasPropostas + "/" + dadosForca.cpf,
-        url: "https://65cc8550-6dac-4e6c-bc80-9b88091f36d5.mock.pstmn.io/materiaisdivulgacao",
-        //url: "http://172.16.244.160:8090/materiaisdivulgacao",
+        url: URLBase + "/corretorservicos/1.0/materiaisdivulgacao/app",
         method: "GET",
         headers: {
             "Content-Type": "application/json",
-            //"Authorization": "Bearer " + Token,
+            "Authorization": "Bearer " + token,
             "Postman-Token": "5299ba38-9752-4f3f-93de-5a6adecf1726"
         },
         success: function (resp) {
@@ -44,138 +46,159 @@ function preencherMateriaisDivulgacao() {
         },
     });
 
-    buscarMateriaisDivulgacao(function (dataMateriais) { //TO DO: adicionar o nome do retorno correto
+    callTokenVendas(function (dataToken) {
 
-        var dataMateriais = JSON.parse(dataMateriais);
+        buscarMateriaisDivulgacao(function (dataMateriais) {
 
-        $.each(dataMateriais.categorias, function (iCategoria, itemCategoria) {
+            $.each(dataMateriais.categoriasMaterialDivulgacao, function (iCategoria, itemCategoria) {
 
-            let componentTitle = getComponent("titleMateriais");
-            componentTitle = componentTitle.replace("{LABEL}", itemCategoria.nome);
-            componentTitle = componentTitle.replace("{DESCRICAO}", itemCategoria.descricao);
-            $("#conteudoPage").append(componentTitle);
+                let componentTitle = getComponent("titleMateriais");
+                componentTitle = componentTitle.replace("{LABEL}", itemCategoria.nome);
+                componentTitle = componentTitle.replace("{DESCRICAO}", itemCategoria.descricao);
+                $("#conteudoPage").append(componentTitle);
 
-            $.each(itemCategoria.subcategorias, function (iSubcategoria, itemSubcategoria) {
+                $.each(itemCategoria.subCategoriasMaterialDivulgacao, function (iSubcategoria, itemSubcategoria) {
 
-                let subCategoriaMateriais = getComponent("subCategoriaMateriais");
-                subCategoriaMateriais = subCategoriaMateriais.replace("{LABEL}", itemSubcategoria.nome);
-                $("#conteudoPage").append(subCategoriaMateriais);
-                //$("#conteudoPage").append('<div style: "white-space: nowrap;" id="' + itemSubcategoria.nome.replaceAll(" ", "-") + '"></div>');
-                
-                let idCarousel = itemSubcategoria.nome.replaceAll(" ", "-");
+                    let subCategoriaMateriais = getComponent("subCategoriaMateriais");
+                    subCategoriaMateriais = subCategoriaMateriais.replace("{LABEL}", itemSubcategoria.nome);
+                    $("#conteudoPage").append(subCategoriaMateriais);
 
-                if (itemSubcategoria.materiaisDivulgacao.length > 0) $("#conteudoPage").append('<div class="carousel" id="' + idCarousel + '"></div>');
+                    let idCarousel = itemSubcategoria.nome.replaceAll(" ", "-");
 
-                $.each(itemSubcategoria.materiaisDivulgacao, function (iMateriais, itemMateriais) {
+                    if (itemSubcategoria.materiaisDivulgacao.length > 0) $("#conteudoPage").append('<div class="carousel" id="' + idCarousel + '"></div>');
 
-                    let boxThumbNail = getComponent("boxMateriaisPlanos");
-                    boxThumbNail = boxThumbNail.replace("{IMAGEM}", 'data:' + itemMateriais.tipoConteudo + ';base64, ' + itemMateriais.thumbnail);
-                    boxThumbNail = boxThumbNail.replace("{NOME}", itemMateriais.nome);
-                    boxThumbNail = boxThumbNail.replace("{CODIGO}", itemMateriais.codigoMaterialDivulgacao);
-                    boxThumbNail = boxThumbNail.replace("{NOMEIMAGEM}", itemMateriais.nome);
-                    boxThumbNail = boxThumbNail.replace("{CODIGOIMAGEM}", itemMateriais.nome);
-                    boxThumbNail = boxThumbNail.replace("{DESCRICAO}", itemMateriais.descricao);
-                    
-                    //boxThumbNail = boxThumbNail.replace("{METODO}", 'downloadSlick($(this))');
-                    $('#' + idCarousel).append(boxThumbNail);
+                    $.each(itemSubcategoria.materiaisDivulgacao, function (iMateriais, itemMateriais) {
+
+                        let boxThumbNail = getComponent("boxMateriaisPlanos");
+                        boxThumbNail = boxThumbNail.replace("{IMAGEM}", 'data:' + itemMateriais.tipoConteudo + ';base64, ' + itemMateriais.thumbnail);
+                        boxThumbNail = boxThumbNail.replace("{NOME}", itemMateriais.nome);
+                        boxThumbNail = boxThumbNail.replace("{CODIGO}", itemMateriais.codigoMaterialDivulgacao);
+                        boxThumbNail = boxThumbNail.replace("{NOMEIMAGEM}", itemMateriais.nome);
+                        boxThumbNail = boxThumbNail.replace("{CODIGOIMAGEM}", itemMateriais.codigoMaterialDivulgacao);
+                        boxThumbNail = boxThumbNail.replace("{DESCRICAO}", itemMateriais.descricao);
+
+                        $('#' + idCarousel).append(boxThumbNail);
+
+                    });
 
                 });
 
             });
 
-        });
-
-        $('.carousel').slick({
-            dots: true,
-            infinite: true,
-            centerMode: true,
-            speed: 700,
-            centerPadding: '40px',
-            autoPlay: true,
-            slidesToShow: 4,
-            slidesToScroll: 4,
-            responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true,
-                    autoPlay: true,
-                    centerPadding: '40px',
-                    speed: 1000,
-                    autoplaySpeed: 500
+            $('.carousel').slick({
+                dots: true,
+                infinite: true,
+                centerMode: true,
+                speed: 700,
+                autoPlay: true,
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true,
+                        autoPlay: true,
+                        speed: 1000,
+                        autoplaySpeed: 500
+                    }
+                }, {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        autoPlay: true,
+                        infinite: true,
+                        dots: true,
+                        speed: 1000,
+                        autoplaySpeed: 500
+                    }
+                }, {
+                    breakpoint: 480,
+                    settings: {
+                        autoPlay: true,
+                        slidesToShow: 1,
+                        autoPlay: true,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true,
+                        speed: 1000,
+                        autoplaySpeed: 500
+                    }
                 }
-            }, {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    autoPlay: true,
-                    infinite: true,
-                    centerPadding: '40px',
-                    dots: true,
-                    speed: 1000,
-                    autoplaySpeed: 500
-                }
-            }, {
-                breakpoint: 480,
-                settings: {
-                    autoPlay: true,
-                    slidesToShow: 1,
-                    autoPlay: true,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    centerPadding: '50px',
-                    dots: true,
-                    speed: 1000,
-                    autoplaySpeed: 500
-                }
-            }
-            ]
-        });
-
-        $(".img-materiais").click(function () {
-
-            swal({
-                title: "Aguarde",
-                text: 'Estamos carregando a imagem',
-                content: "input",
-                imageUrl: "img/load.gif",
-                showCancelButton: false,
-                showConfirmButton: false,
-                icon: "info",
-                button: {
-                    text: "...",
-                    closeModal: false,
-                },
+                ]
             });
 
-            getImagemFull(function (dataImage) {
+            $(".img-materiais").click(function () {
 
-                var dataImage = JSON.parse(dataImage);
+                let codigoImagem = $(this).data("codigo");
 
-                let extensao = dataImage.tipoConteudo.split("/")[1];
-                let nomeArquivo = dataImage.nome.replace("." + extensao, "").trim();
+                swal({
+                    title: "Aguarde",
+                    text: 'Estamos carregando a imagem',
+                    content: "input",
+                    imageUrl: "img/load.gif",
+                    showCancelButton: false,
+                    showConfirmButton: false,
+                    icon: "info",
+                    button: {
+                        text: "...",
+                        closeModal: false,
+                    },
+                });
 
-                console.log(extensao);
-                console.log(nomeArquivo);
-                $("#imagem").html('<img class="img-responsive center-block imagemDownload" src="' + 'data:' + dataImage.tipoConteudo + ';base64, ' + dataImage.arquivo + '" data-extensao = "' + extensao + '" data-nome = "' +
-                    nomeArquivo  + '" />' +
+                callTokenVendas(function (dataToken) {
 
-                    '<div><label class="descricaoSlick">' + dataImage.descricao + '</label><div><a class="downloadSlick" onclick="downloadImage()">Download</a></div></div>');
+                    if (dataToken.status == undefined) {
 
-                swal.close();
-                $('#myModal').modal('show');
-            
-            }, "dadada", $(this).data("codigo"))
+                        getImagemFull(function (dataImage) {
 
-        });
+                            if (dataImage.status == undefined) {
 
-        downloadSlick();
-        swal.close();
+                                let extensao = dataImage.tipoConteudo.split("/")[1];
+                                let nomeArquivo = dataImage.nome.replace("." + extensao, "").trim();
 
-    }, "saasdasdasdd");
+                                console.log(extensao);
+                                console.log(nomeArquivo);
+                                $("#imagem").html('<img class="img-responsive center-block imagemDownload" src="' + 'data:' + dataImage.tipoConteudo + ';base64, ' + dataImage.arquivo + '" data-extensao = "' + extensao + '" data-nome = "' +
+                                    nomeArquivo + '" />' +
+
+                                    '<div><label class="descricaoSlick">' + dataImage.descricao + '</label><div><a class="downloadSlick" onclick="downloadImage()">Download</a></div></div>');
+
+                                swal.close();
+                                $('body').addClass('stop-scrolling');
+                                $('#myModal').modal('show');
+
+
+                            } else {
+
+                                swal("Ops!", "Erro no carregamento da imagem", "error");
+                                return;
+
+                            }
+
+                        }, dataToken.access_token, codigoImagem)
+
+                    } else {
+
+                        swal("Ops!", "Erro no carregamento da imagem", "error");
+                        return;
+
+                    }
+
+                });
+
+            });
+
+
+            downloadSlick();
+            swal.close();
+
+        }, dataToken.access_token);
+
+    });
 }
 
 function downloadSlick() {
@@ -196,14 +219,36 @@ function downloadSlick() {
             },
         });
 
-        getImagemFull(function (dataImage) {
+        let codigoImagem = $(this).data("codigo");
 
-            var dataImage = JSON.parse(dataImage);
+        callTokenVendas(function (dataToken) {
 
-            ob.gerarArquivo(dataImage.arquivo.trim(), dataImage.nome, dataImage.tipoConteudo.split("/")[1]);
-            swal.close();
+            if (dataToken.status == undefined) {
 
-        }, "dadada", $(this).data("codigo"))
+                getImagemFull(function (dataImage) {
+
+                    if (dataImage.status == undefined) {
+
+                        ob.gerarArquivo(dataImage.arquivo.trim(), dataImage.nome, dataImage.tipoConteudo.split("/")[1]);
+                        swal.close();
+
+                    } else {
+
+                        swal("Ops!", "Erro no carregamento da imagem", "error");
+                        return;
+
+                    }
+
+                }, dataToken.access_token, codigoImagem)
+
+            } else {
+
+                swal("Ops!", "Erro no carregamento da imagem", "error");
+                return;
+
+            }
+
+        });
 
     });
 }
@@ -222,14 +267,11 @@ function getImagemFull(callback, token, codigoDaImagem) {
 
     $.ajax({
         async: true,
-        //url: URLBase + "/corretorservicos/1.0/dashboardPropostaPME/" + statusTodasPropostas + "/" + dadosForca.cpf,
-        url: "https://c42d3c16-88e2-4ec3-b461-495a0af2ebfc.mock.pstmn.io/materialfull",
-        //url: "http://172.16.244.160:8090/materialdivulgacao/1",
+        url: URLBase + "/corretorservicos/1.0/materialdivulgacao/" + codigoDaImagem,
         method: "GET",
         headers: {
             "Content-Type": "application/json",
-            //"Authorization": "Bearer " + Token,
-            "Postman-Token": "5299ba38-9752-4f3f-93de-5a6adecf1726"
+            "Authorization": "Bearer " + token
         },
         success: function (resp) {
             callback(resp);
