a9fde255356b605ab4eb58c56d49373f88e7d7f7
Yago Almeida Santos <yalmeida@vectoritcgroup.com>
2018-07-25 19:35:45 -0300

    Criada estrutura de resumo status proposta pme

--- VendasOdontoPrev/app/src/main/assets/app/resumoStatusPropostaPmeController.js
+++ VendasOdontoPrev/app/src/main/assets/app/resumoStatusPropostaPmeController.js
@@ -55,7 +55,9 @@ function popularCamposProposta() {
             return;
         }
 
-        //var dataEmpresa = JSON.parse(dataEmpresa);
+        console.log(dataEmpresa);
+
+        var dataEmpresa = JSON.parse(dataEmpresa);
 
         console.log(dataEmpresa);
 
@@ -75,7 +77,7 @@ function popularCamposProposta() {
 
         }
 
-        // Preenchimento do cep
+        // Preenchimento do endereço da empresa
         $("#cepEmpresa").html(dataEmpresa.enderecoEmpresa.cep);
         $("#enderecoEmpresa").html(dataEmpresa.enderecoEmpresa.logradouro);
         $("#complementoEmpresa").html(dataEmpresa.enderecoEmpresa.complemento);
@@ -84,7 +86,36 @@ function popularCamposProposta() {
         $("#cidadeEmpresa").html(dataEmpresa.enderecoEmpresa.cidade);
         $("#estadoEmpresa").html(dataEmpresa.enderecoEmpresa.estado);
 
+        //Preenchimento das datas operacionais
+        $("#vencimentoEmpresa").html(dataEmpresa.vencimentoFatura);
+        $("#dataMovimentacaoEmpresa").html(dataEmpresa.dataMovimentacao);
+        $("#dataVigenciaEmpresa").html(dataEmpresa.dataVigencia);
+
         swal.close();
+
+        let tamanhoDaPagina = getRepository("paginacaoBeneficiarios").pageSize;
+
+        console.log("Tamanho da pagina: " + tamanhoDaPagina);
+
+        callBeneficiariosList(function (dataListBeneficiarios) {
+
+            var dataListBeneficiarios = JSON.parse(dataListBeneficiarios);
+            console.log(dataListBeneficiarios);
+
+            $.each(dataListBeneficiarios.titulares, function (i, item) {
+
+                var componentBoxBeneficiarios = getComponent("boxBeneficiarios");
+                var componentBoxBeneficiarios = componentBoxBeneficiarios.replace("{NOME-TITULAR}", item.nome);
+                var componentBoxBeneficiarios = componentBoxBeneficiarios.replace("{NUMERO}", item.dependentes.length);
+
+                $("#boxBeneficiarios").append(componentBoxBeneficiarios);
+                
+            });
+
+            $("#mostrarMais").attr("data-contador", dataListBeneficiarios.numPagina);
+            $("#mostrarMais").attr("data-contador", dataListBeneficiarios.numPagina);
+
+        }, "dsadada", cdEmpresa, 4, tamanhoDaPagina)
 
     }, "dasdasdsada", cdEmpresa);
 
@@ -99,8 +130,8 @@ function callDadosEmpresa(callback, token, cdEmpresa) {
 
     $.ajax({
         async: true,
-        //url: "https://6a428f33-b87b-43d0-92ef-7fdc244530ea.mock.pstmn.io" + "/empresa/" + cdEmpresa,
-        url: "http://172.16.244.160:8090/empresa/" + cdEmpresa,
+        url: "https://6a428f33-b87b-43d0-92ef-7fdc244530ea.mock.pstmn.io" + "/empresa/" + cdEmpresa,
+        //url: "http://172.16.244.160:8090/empresa/" + cdEmpresa,
         method: "GET",
         headers: {
             "Content-Type": "application/json",
@@ -116,3 +147,26 @@ function callDadosEmpresa(callback, token, cdEmpresa) {
     });
 
 }
+
+function callBeneficiariosList(callback, token, cdEmpresa, numeroDaPagina, tamanhoDaPagina) {
+
+    $.ajax({
+        async: true,
+        url: "https://8c85a4e9-ad01-4ff8-ab22-6a250b741bfe.mock.pstmn.io/beneficiarios" + "/empresa/" + cdEmpresa + "?numPag=" + numeroDaPagina + "&tamPag=" + tamanhoDaPagina,
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Cache-Control": "no-cache",
+            "Authorization": "Bearer " + token
+        },
+        success: function (resp) {
+            callback(resp);
+        },
+        error: function (xhr) {
+            callback(xhr);
+        }
+    });
+
+}
+
+
