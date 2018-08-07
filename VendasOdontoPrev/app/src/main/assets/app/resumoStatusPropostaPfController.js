6c17cfeb8e36b9fd55fa0e96b16c849b1dcfa691
Yago Almeida Santos <yalmeida@vectoritcgroup.com>
2018-08-07 18:48:15 -0300

    Alterada comparacao para cdStatus

--- VendasOdontoPrev/app/src/main/assets/app/resumoStatusPropostaPfController.js
+++ VendasOdontoPrev/app/src/main/assets/app/resumoStatusPropostaPfController.js
@@ -222,7 +222,7 @@ function popularCamposProposta() {
     if (resumoProposta.dadosBancarios.agencia == "" && resumoProposta.dadosBancarios.conta == "") {
 
         $("#formaPagamento").html("Boleto");
-        if (resumoProposta.status.toUpperCase() == "PROPOSTA CONCLUÍDA COM SUCESSO") $(".btnPadBtmTop").removeClass("hide");
+        if (resumoProposta.cdStatusVenda == 3) $(".btnPadBtmTop").removeClass("hide");
     } else {
         $("#formaPagamento").html("Débito em Conta");
     }
