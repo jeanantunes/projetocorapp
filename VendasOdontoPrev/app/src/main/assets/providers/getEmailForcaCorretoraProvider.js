b1477069f27359aa359b9b787eba026db4160a6f
Yago Almeida Santos <yalmeida@vectoritcgroup.com>
2018-10-10 09:47:41 -0300

    COR-892 Criado provider para consumir servico "/forcavenda/{cdForcaVenda}/email"

---
+++ VendasOdontoPrev/app/src/main/assets/providers/getEmailForcaCorretoraProvider.js
@@ -0,0 +1,24 @@
+﻿var metodoApi = "/forcavenda/{cdForcaVenda}/email";
+
+function getEmailForcaCorretora(accessToken, cdForcaVenda, callbackSuccess, callbackError) {
+
+    var url = URLBase + apiGateway + metodoApi.replace("{cdForcaVenda}", cdForcaVenda);
+
+    $.ajax({
+        async: true,
+        url: url,
+        method: "GET",
+        headers: {
+            "Authorization": "Bearer " + accessToken,
+            "Content-Type": "application/json",
+            "Cache-Control": "no-cache"
+        },
+        success: function (resp) {
+            callbackSuccess(resp);
+        },
+        error: function (xhr) {
+            callbackError(xhr);
+        }
+    });
+
+}
